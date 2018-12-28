7777341787a81c3385f8fb0d740a12e2ae0d8de6
Jesse Glick <jglick@cloudbees.com>
2018-12-27 20:18:44 -0500

    Some test coverage.

---
+++ src/test/java/com/github/jenkinsx/quickstarts/vertx/rest/prometheus/DemoVerticleTest.java
@@ -0,0 +1,34 @@
+package com.github.jenkinsx.quickstarts.vertx.rest.prometheus;
+
+import static io.vertx.core.Vertx.vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientRequest;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(VertxUnitRunner.class)
+public class DemoVerticleTest {
+
+    @Test
+    public void stuff(TestContext context) {
+        DemoVerticle.main(null);
+        HttpClient client = vertx().createHttpClient();
+        assert200(client, context, "/hello");
+        assert200(client, context, "/actuator/health");
+    }
+    
+    private void assert200(HttpClient client, TestContext context, String uri) {
+        Async async = context.async();
+        HttpClientRequest req = client.get(8080, "localhost", uri);
+        req.exceptionHandler(err -> context.fail(err.getMessage()));
+        req.handler(resp -> {
+            context.assertEquals(200, resp.statusCode());
+            async.complete();
+        });
+        req.end();
+    }
+
+}
